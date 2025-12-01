7aaef3e4267fba698887950e91c656189bf80d97
joco9822-alt <joco9822@gmail.com>
2025-12-01 20:09:51 +0900

    :sparkles: [Feat] Add sleep() to observe process information with ps command

--- 01_process/fork.c
+++ 01_process/fork.c
@@ -1,12 +1,18 @@
 #include <stdio.h>      
+#include <stdlib.h>     // sleep() 
 #include <unistd.h>     // Linux System Call: fork(), getpid(), getppid() 
 #include <sys/types.h>  // Type: pid_t 
 
 int main() {
-    printf("1. [부모] 자식을 낳기 전입니다.\n");
+    printf("1. [부모] 자식을 낳기 전입니다. (10초 대기)\n");
 
-    pid_t pid = fork(); // 분기점
-
+    for(int i = 0; i < 10; i++) {
+        printf("%d초\n", i);
+	sleep(1);
+    } 
+    
+    pid_t pid = fork(); 
+ 
     if (pid == 0) {
         // 자식 프로세스
         printf("2. [자식] 응애! 내 PID: %d, 부모 PID: %d\n", getpid(), getppid());
@@ -14,5 +20,9 @@ int main() {
         // 부모 프로세스
         printf("3. [부모] 자식(PID: %d)을 낳았습니다.\n", pid);
     }
+
+    sleep(30); 
+
     return 0;
 }
+
